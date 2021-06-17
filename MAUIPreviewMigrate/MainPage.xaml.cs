d102ed66530a086c46c69f77d5caaf97eaecd5a9
yamachu <yamachu.dev@gmail.com>
2021-06-17 20:31:50 +0900

    Add maui-blazor template app generated by Microsoft.Maui.Templates::6.0.100-preview.4.634

---
+++ MAUIPreviewMigrate/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace MAUIPreviewMigrate
+{
+	public partial class MainPage : ContentPage, IPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ MAUIPreviewMigrate/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace MAUIPreviewMigrate
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
